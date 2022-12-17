ebc7dad8a00f87f3412dfb55aed08f07ef0c4a6c
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-17 14:23:05 -0500

    added product model

--- models/Product.js
+++ models/Product.js
@@ -2,14 +2,47 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    product_name: {
+      type: DataTypes.String,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.decimal,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.Integer,
+      allowNull: true,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -21,3 +54,5 @@ Product.init(
 );
 
 module.exports = Product;
+
+
