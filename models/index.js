a92c7361baa00bc0075a0386e71f60845e1b0a69
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-12-19 20:07:28 -0500

    category routes: get, post, delete

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'id',
+  onDelete: 'CASCADE',
 });
 
 // Categories have many Products
